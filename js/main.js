d5088e5b0c1bf8e702d3a667cc67c197e816667b
Diego Almeida <diego.almeida@outlook.com>
2020-09-25 22:36:42 -0300

    Added 4.1.1 & 2.4.1 - Compatible and Navigable Guideline

--- js/main.js
+++ js/main.js
@@ -16,15 +16,13 @@ function getUserPosition() {
 
 function fetchApi(url = defaultUrl) {
     let city = document.querySelector('.city');
-    let temp = document.querySelector('span');
     fetch(url)
         .then((data) => {
             return data.json();
         })
         .then((data) => {
             let tempInCelsius = ((5 / 9) * (data.main.temp - 32)).toFixed(1);
-            city.innerText = `Hoje a temperatura em ${data.name} é:`;
-            temp.innerText = tempInCelsius;
+            city.innerText = `Hoje a temperatura em ${data.name} é de ${tempInCelsius}ºC`;
         })
         .catch((err) => {
             city.innerText = `Impossível acessar o OpenWeather. Verifique a sua conexão.`;
